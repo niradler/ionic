daafa86f47b297b9a847d109e9d957fff7ba389e
Nir Adler <niradler55@gmail.com>
2016-09-02 22:55:55 +0300

    add firebase

--- www/js/services.js
+++ www/js/services.js
@@ -1,9 +1,17 @@
 angular.module('app.services', [])
-
-.factory('BlankFactory', [function(){
-
-}])
+.factory("Items", function($firebaseArray) {
+  var itemsRef = new Firebase("https://ionic-store-2c979.firebaseio.com/products");
+  return $firebaseArray(itemsRef);
+})
 
 .service('BlankService', [function(){
 
 }]);
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCepLx7DcY5D3LVdm1vsZDeSE9HJ7ajXl0",
+    authDomain: "ionic-store-2c979.firebaseapp.com",
+    databaseURL: "https://ionic-store-2c979.firebaseio.com",
+    storageBucket: "ionic-store-2c979.appspot.com",
+  };
